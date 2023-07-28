5b677af52a3bdd70310a01ff367ebb61a4db4047
RomanG <roma@gmail.com>
2023-07-28 15:01:12 +0300

    add shouldFindSelenideRepositorySearchAtTheTop

---
+++ src/test/java/github/ContributorToSelenide.java
@@ -0,0 +1,31 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ContributorToSelenide {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void andreySolncevFirstContributor() {
+        open("https://github.com/selenide/selenide");
+        $("div.Layout-sidebar").$(byText("Contributors"))
+                .closest("h2").sibling(0).$$("li").first().hover();
+//        $(".BorderGrid-cell .list-style-none [href=https://github.com/asolntsev]").shouldHave(text("asolntsev"));
+        $(".Popover-message").shouldHave(text("Andrei Solntsev"));
+//        $$(".Popover ").findBy(visible).shouldHave(text("Andrei Solntsev"));
+        sleep(5000);
+
+    }
+}
--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -4,6 +4,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SelenideRepositorySearch {
 
     @BeforeAll
@@ -14,6 +18,13 @@ public class SelenideRepositorySearch {
 
     @Test
     void shouldFindSelenideRepositorySearchAtTheTop() {
+        open("https://github.com/");
+        $("button.header-search-button").click();
+        $("#query-builder-test").setValue("Selenide").pressEnter();
+//        $(byAttribute("data-testid","results-list")).$(byAttribute("href","/selenide/selenide")).$("em").shouldHave(text("selenide")).click();
+        $("[data-testid=results-list]").$("em").shouldHave(text("selenide")).click();
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        sleep(3000);
 
     }
 }
